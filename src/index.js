3d6f7647e53f3b0bfbaf7c2d77a7ca1537cf8f45
AtanasovCode <a_ljupce@yahoo.com>
2022-11-18 15:48:29 +0100

    Initialized App

--- src/index.js
+++ src/index.js
@@ -18,7 +18,19 @@ import {
   getAuth,
 } from 'firebase/auth';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBv8_kXlTABhgWR9xfAbz8z0wyuAarpSeo",
+  authDomain: "fir-app-7825e.firebaseapp.com",
+  projectId: "fir-app-7825e",
+  storageBucket: "fir-app-7825e.appspot.com",
+  messagingSenderId: "215741555944",
+  appId: "1:215741555944:web:4609c03864f426dcb4d55d",
+  measurementId: "G-SMS3MMG4GT"
+};
+
+initializeApp(firebaseConfig);
 
 const db = getFirestore();
 const auth = getAuth();
